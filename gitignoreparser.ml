acefa68fd8b204d3d90ecf485f8f67586eb51a50
dhilst <danielhilst@gmail.com>
2019-08-11 11:19:10 -0300

    Treat files and directories accordingly

--- gitignoreparser.ml
+++ gitignoreparser.ml
@@ -3,6 +3,8 @@ open Core
 type t =
   | Neg of t
   | Pattern of Re.re
+  | File of string
+  | Directory of string
 
 let rec compile = function%pcre
   | {|^!(?<rest>.*)|} -> Neg (compile rest)
@@ -10,18 +12,26 @@ let rec compile = function%pcre
   | {|^(?<prefix>.*?)\*\*(?<suffix>.*)|} ->
       let open Re.Pcre in
       let prefix = quote prefix in
-      let suffix = quote suffix in 
+      let suffix = quote suffix in
       let pattern = prefix ^ {|.*|} ^ suffix in
       let r = regexp pattern in
       Pattern r
-  | {|?<line>.*?\*.*|} -> 
+  | {|?<line>.*?\*.*|} ->
       let r = Re.Glob.glob line |> Re.compile in
       Pattern r
-  | _ -> raise @@ Failure ""
+  | {|^(?<line>.*?/)|} -> Directory line
+  | line -> File line
 
-let rec matches pattern text = match pattern with
-  | Neg a -> not(matches a text)
-  | Pattern a -> (match Re.exec_opt a text with
+let is_in_directory file directory =
+  match%pcre file with
+  | {|^(?<path>.*/)|} -> path = directory
+  | _ -> false
+
+let rec matches pattern file = match pattern with
+  | Neg a -> not(matches a file)
+  | File a -> a = file
+  | Directory a -> is_in_directory file a
+  | Pattern a -> (match Re.exec_opt a file with
     | Some _ -> true
     | None -> false)
 
@@ -32,3 +42,5 @@ let%test _ = matches (compile "*.sw[op]") "foo.swo"
 let%test _ = matches (compile "~*") "~foo"
 let%test _ = not @@ matches (compile "!foo/*/bar") "foo/xxx/bar"
 let%test _ = not @@ matches (compile "foo/*/bar") "foo/xxx/yyy/bar"
+let%test _ = matches (compile "foo/") "foo/"
+let%test _ = matches (compile "foo/") "foo/bar"
